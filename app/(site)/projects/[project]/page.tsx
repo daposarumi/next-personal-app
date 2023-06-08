c3a11f86340f175ed38047e126d67e956ba16ac5
daposarumi <sarumiadedapo@gmail.com>
2023-06-08 23:56:17 +0100

    new

--- app/(site)/projects/[project]/page.tsx
+++ app/(site)/projects/[project]/page.tsx
@@ -15,7 +15,7 @@ export default async function Project({ params }: Props) {
     return (
     <div>
         <header className="flex items-center justify-between">
-            <h1 className="bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent lg:text-5xl sm:text-3xl drop-shadow font-extrabold">{project.name}</h1>
+            <h1 className="text-neutral-700 lg:text-5xl sm:text-4xl drop-shadow font-extrabold">{project.name}</h1>
             <a href={project.url} title="View Project" target="_blank" rel="noopener noreferrer" className="bg-gray-100 rounded-lg text-gray-500 font-bold py-3 px-4 whitespace-nowrap hover:bg-pink-500 hover:text-pink-100 transition">
                 View Project
             </a>
