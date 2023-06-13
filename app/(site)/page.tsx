51f2f62336d3a2045d43b03a5a326a62dbbbaaf5
daposarumi <sarumiadedapo@gmail.com>
2023-06-13 13:59:07 +0100

    new

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -8,9 +8,9 @@ export default async function Home() {
 
   return (
     <div>
-      <h1 className="text-7xl font-extrabold">Hi I&apos;m <span className="bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent">Adedapo!</span></h1>
-      <p className="mt-4 text-xl text-gray-600 max-w-2xl">These are my most recent front-end projects. Check them out and let me know what you think</p>
-      
+      <h1 className="text-7xl font-extrabold">Hi I&apos;m <span className="bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent">Adedapo</span>, a prolific frontend engineer</h1>
+      <p className="mt-4 text-xl text-gray-600 max-w-2xl">I&apos;m passionate about creating user-centred systems, it&apos;s what I do best. These are my most recent front-end projects. Check them out and let me know what you think. Also, i look forward to working with you</p>
+      <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">Hire Me</button>
       <h2 className="mt-24 font-bold text-gray-700 text-3xl">My Projects</h2>
       <div className="mt-5 grid md: grid-cols-2 lg:grid-cols-2 gap-8">
       {projects.map((project) => (
