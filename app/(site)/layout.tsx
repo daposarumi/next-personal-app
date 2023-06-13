25a9cffcf1bb678b25751d303264099b02759fbc
daposarumi <sarumiadedapo@gmail.com>
2023-06-13 13:25:17 +0100

    new projects

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -19,7 +19,7 @@ export default async function RootLayout({
 
   return (
     <html lang="en">
-      <body className='max-w-4xl mx-auto py-10 px-6'>
+      <body className='max-w-4xl mx-auto py-10'>
         <header className='flex items-center justify-between'>
           <Link href="/"
           className="bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent text-lg font-bold">
